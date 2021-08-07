319d74b06a3061111f62933296052111cb855ff3
Manuel928 <Afrikareportersng@gmail.com>
2021-08-07 04:33:05 -0700

    Add project files.

---
+++ CLCalc/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CLCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+           double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Select an operator " + "(+ - * / %): ");
+
+            switch (Console.ReadLine())
+            {
+                case "+":
+                    Console.WriteLine($"RESULT = {num1 + num2}");
+                    break;
+                case "-":
+                    Console.WriteLine($"RESULT = {num1 - num2}");
+                    break;
+                case "*":
+                    Console.WriteLine($"RESULT = {num1 * num2}");
+                    break;
+                case "/":
+                    Console.WriteLine($"RESULT = {num1 / num2}");
+                    break;
+                case "%":
+                    Console.WriteLine($"RESULT = {num1 % num2}");
+                    break;
+            }
+                
+        }
+    }
+}
